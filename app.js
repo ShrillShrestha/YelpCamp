5b522ecce56bcc1d93f91e7f700cb5ddefb7b792
Shrill Shrestha <shrill.shrestha@gmail.com>
2019-11-19 07:51:45 -0600

    Feat: comment feature

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express"),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
     Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
     seedDB = require("./seeds");
 
 var app = express();
@@ -21,7 +22,7 @@ app.get("/campgrounds", function(req, res){
         if(err){
             console.log(err);
         }else{
-            res.render("index", {campgrounds: campdata});
+            res.render("campgrounds/index", {campgrounds: campdata});
         }
     });
 });
@@ -41,7 +42,7 @@ app.post("/campgrounds", function(req, res){
 });
 
 app.get("/campgrounds/new", function(req, res){
-    res.render("new");
+    res.render("campgrounds/new");
 });
 
 app.get("/campgrounds/:id", function(req, res){
@@ -49,9 +50,37 @@ app.get("/campgrounds/:id", function(req, res){
         if(err){
             console.log(err);
         }else{
-            res.render("show", {campground: foundCampground});
+            res.render("campgrounds/show", {campground: foundCampground});
         }
     });
+});
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log("Error!");
+        }else{
+            res.render("comments/new", {campground: campground});
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        }else{
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                }else{
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/"+ campground._id);
+                }
+            });
+        }
+    })
 });
 
 app.listen(3000, function(){
